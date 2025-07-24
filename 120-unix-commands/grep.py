bb9c37554fda4501cb129276264b04ab4a097616
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-07-24 14:03:50 +0100

    Groom, black, standardise

--- 120-unix-commands/grep.py
+++ 120-unix-commands/grep.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
-
 import sys
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     try:
         file_name = sys.argv[2]
         pattern = sys.argv[1]
 
-        with open(file_name, 'r') as infile:
+        with open(file_name, "r") as infile:
             for line in infile:
                 if pattern in line:
-                    print(line, end='')
-    except IndexError:
-        print(f'{sys.argv[0]}: Missing required argument.')
-    except FileNotFoundError:
-        print(f'{sys.argv[0]}: Cannot open "{sys.argv[2]}"')
+                    print(line, end="")
+    except (
+        IndexError,
+        FileNotFoundError,
+    ):
+        print(f"Usage: python3 {sys.argv[0]} <file to sarch> <pattern>")
--- 120-unix-commands/nl.py
+++ 120-unix-commands/nl.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 
-
 import sys
 
 
-if __name__ == '__main__':
-
-    line_no = 1
-
+if __name__ == "__main__":
     try:
-        with open(sys.argv[1], 'r') as infile:
+        with open(sys.argv[1], "r") as infile:
+            line_no = 1
             for line in infile:
-                print(f'{line_no:5} {line}', end='')
+                print(f"{line_no:5} {line}", end="")
                 line_no += 1
-    except IndexError:
-        print(f'{sys.argv[0]}: Missing required argument.')
-    except FileNotFoundError:
-        print(f'{sys.argv[0]}: Cannot open "{sys.argv[1]}"')
+    except (
+        IndexError,
+        FileNotFoundError,
+    ):
+        print(f"Usage: python3 {sys.argv[0]} <file to display>")
