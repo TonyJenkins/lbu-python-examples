867aa08672ff98374af2d0ba4c5f90d954cb917c
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-07-24 14:23:00 +0100

    Groom, black, typos

--- 150-speed-camera/conf.py
+++ 150-speed-camera/conf.py
@@ -1,15 +1,9 @@
-
 #!/usr/bin/env python3
 
 
-VEHICLE_TYPES = 'CVHB'
+VEHICLE_TYPES = "CVHB"
 
-SPEED_LIMITS = {
-    'C': 60,
-    'V': 50,
-    'H': 40,
-    'B': 40
-}
+SPEED_LIMITS = {"C": 60, "V": 50, "H": 40, "B": 40}
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
