22040a3e9a9374ece7d6ae7ebd805dea7c197946
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-07-24 12:01:56 +0100

    Groom, fix example, Black

--- 060-dry-code/file_checker.py
+++ 060-dry-code/file_checker.py
@@ -2,20 +2,20 @@
 
 import sys
 
+
 def check_file(file_to_check):
     from os import listdir
     from os.path import isfile
 
-    files_that_exist = [f for f in listdir('.') if isfile(f)]
+    files_that_exist = [f for f in listdir(".") if isfile(f)]
     return file_to_check in files_that_exist
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         if check_file(sys.argv[1]):
             print(f'"{sys.argv[1]}" found!')
         else:
             print(f'"{sys.argv[1]}" not found!')
     except IndexError:
-        print(f'Usage: python3 {sys.aegv[0]} <file to check>')
-
+        print(f"Usage: python3 {sys.aegv[0]} <file to check>")
--- 060-dry-code/many_sided_die.py
+++ 060-dry-code/many_sided_die.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 
-
 from random import randint
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     try:
-        sides = int(input('How many sides does the die have? '))
+        sides = int(input("How many sides does the die have? "))
         if sides < 2:
-            raise ValueError('Number of sides must be at least 2.')
+            raise ValueError("Number of sides must be at least 2.")
     except ValueError:
-        print('Invalid input. Please enter a positive integer.')
+        print("Invalid input. Please enter a positive integer.")
     else:
         result = randint(1, sides)
-        print(f'The die landed on: {result}')
-    
+        print(f"The die landed on: {result}")
